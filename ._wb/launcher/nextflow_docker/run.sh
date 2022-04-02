1329a6e4994690104378327262bdeaa6047b9623
Sam Minot <sminot@fredhutch.org>
2022-04-02 07:21:42 -0700

    Add launcher/nextflow_docker

---
+++ ._wb/launcher/nextflow_docker/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+echo "Setting up nextflow.config"
+
+echo """
+{
+    docker {
+        enabled = true
+    }
+}
+""" > nextflow.config
+
+# Execute the tool in the local environment
+/bin/bash ._wb/helpers/run_tool
