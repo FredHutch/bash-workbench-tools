b887a85d905e22c546333cdee53b103b1855ae18
Sam Minot <sminot@fredhutch.org>
2023-07-26 12:25:19 -0700

    Add strainphlan

---
+++ ._wb/tool/strainphlan/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+WORKFLOW_REPO="FredHutch/metaphlan-nf"
+
+date
+echo
+echo "Running workflow '${WORKFLOW_REPO}' from ${PWD}"
+echo
+
+# Run the workflow
+echo Starting workflow
+nextflow \
+    run \
+    "${WORKFLOW_REPO}" \
+    -r main \
+    --output "${PWD}" \
+    -params-file ._wb/tool/params.json \
+    -resume \
+    -latest
+
+# If temporary files were not placed in a separate location
+if [ -d work ]; then
+    # Delete the temporary files created during execution
+    echo Removing temporary files
+    rm -r work
+fi
+
+echo
+date
+echo Done
